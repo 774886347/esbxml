244f0b9667d82b8990a696188f3048f2d5d3fb90
wings <xxxxl>
2021-03-03 07:23:42 +0800

    first commit

---
+++ src/main/java/esbxml/EsbXmlApplication.java
@@ -0,0 +1,13 @@
+package esbxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsbXmlApplication.class, args);
+    }
+
+}
